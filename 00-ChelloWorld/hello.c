d0e23cf1817cdb4b373a512cec3063f905bec810
StefanoPellegrino1 <130502768+StefanoPellegrino1@users.noreply.github.com>
2023-04-18 09:12:27 -0300

    Add files via upload

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 int main(){
-    FILE *file = fopen("output.txt", "wt");
-    fputs("Hello, world!",file);
-    fclose(file);
-
+   printf("Hello world!\n");
+   return 0;
 }
